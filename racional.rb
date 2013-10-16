1120f0df333b8acf06a0405f5f5dd622a3d8d746
Sergio Martín <alu0100611625@ull.edu.es>
2013-10-16 20:00:40 +0100

    Añadidos Métodos a Implementar

--- racional.rb
+++ racional.rb
@@ -9,14 +9,24 @@ def initialize(x,y)
 	
 end
 
-# Convierte a String los Objetos Racional
+# to_s # devuelve una cadena con la representación del racional
+
 def to_s
 	puts "(#{@x}/#{@y})"
 end
+   
+#suma # devuelve un nuevo racional que suma al objeto que invoca el que le pasan como parámetro
+
+#resta # devuelve un nuevo racional que resta al objeto que invoca el que le pasan como parámetro
+
+#producto # devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+
+#division # devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+
 
 end
 
 P1= Racional.new("1","2")
-
+puts gcd(6,3)
 P1.to_s
 
